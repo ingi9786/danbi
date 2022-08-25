7d2ca3076b70b307354908acabb06d921345653f
ingi9786 <ingi9786@naver.com>
2022-08-25 12:37:14 +0900

    [#16] [feat] : 잘못된 날짜가 쿼리로 들어올경우 예외 description : -

--- danbi_proj/routine_app/apis.py
+++ danbi_proj/routine_app/apis.py
@@ -31,7 +31,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import renderer_classes, action
 from rest_framework.renderers import JSONRenderer
-from .utils import date_day, get_today
+from .utils import convert_day, get_today, is_valid_date
 
 
 class RoutineViewSet(viewsets.GenericViewSet):
@@ -102,7 +102,7 @@ class RoutineViewSet(viewsets.GenericViewSet):
         query.delete()
         return Response({"message":msg})
     
-    # 유저가 갖고 있는 outine 전부 보여주기 
+ 
     def list(self, request):
         queryset = self.get_queryset().all()
         serializer = RoutineSerializer(queryset, many=True)
@@ -111,8 +111,9 @@ class RoutineViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=["GET"])
     def days(self, request):
         queryset = self.get_queryset().all() # 유저로 특정된 routine들이 있음.
-        date = request.GET.get('date', get_today())
-        _date = date_day(date)
+        date = request.GET.get('date', 'None') 
+        _date = convert_day(date) if is_valid_date(date) else convert_day(get_today())
+        
         day_list = []
         for query in queryset:
             r_id = query.routine_id
--- danbi_proj/routine_app/utils.py
+++ danbi_proj/routine_app/utils.py
@@ -1,6 +1,7 @@
 from datetime import date
 
-def date_day(arg):
+# Convert 날짜(str) to 요일(str)
+def convert_day(arg):
     daydic = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
     y = int(arg[:4])
     m = int(arg[4:6])
@@ -9,5 +10,12 @@ def date_day(arg):
     _day  = date.weekday(_date)
     return daydic[_day]
 
+# 오늘날짜를 반환하는 함수
 def get_today():
     return "".join(str(date.today()).split('-'))
+
+# date param이 유효한지 확인
+def is_valid_date(arg):
+    return True if arg.isdigit() and len(arg)==8 else False
+
+             
